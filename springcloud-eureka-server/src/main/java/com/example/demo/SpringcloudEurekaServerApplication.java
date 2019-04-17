821611bae22e0ff5288bca8dff8aa19d37150679
EricChang82 <changlejc@Hotmail.com>
2019-04-17 09:46:25 +0800

    1

---
+++ springcloud-eureka-server/src/main/java/com/example/demo/SpringcloudEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer // 启用 eureka server 相关默认配置
+@SpringBootApplication
+public class SpringcloudEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudEurekaServerApplication.class, args);
+	}
+
+}
